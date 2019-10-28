fee85f134ad26e4027bb289f0ef6433785e1b5fc
AnuragAnalog <anurag.peddi1998@gmail.com>
2019-10-28 18:02:06 +0530

    new functionality in progress

--- test-case-generator.py
+++ test-case-generator.py
@@ -24,9 +24,12 @@ def print_matrix(arr: list, r: int, c: int) -> None:
 
     return
 
-def numbers(n: int, mini: int, maxi: int, n_flag: bool =False, dis: bool =False) -> None:
+def numbers(n: int, mini: int, maxi: int, n_flag: bool =False, dis: bool =False, fh, f_flag: bool =False) -> None:
     if n_flag:
-        print(n)
+        if f_flag:
+            fh.write(n)
+        else:
+            print(n)
 
     if dis:
         tmp = list()
@@ -35,16 +38,25 @@ def numbers(n: int, mini: int, maxi: int, n_flag: bool =False, dis: bool =False)
             while val in tmp:
                 val = np.random.randint(mini, maxi+1)
             tmp.append(val)
-            print(val)
+            if f_flag:
+                fh.write(val)
+            else:
+                print(val)
     else:
         for i in range(n):
-            print(np.random.randint(mini, maxi+1))
+            if f_flag:
+                fh.write(np.random.randint(mini, maxi+1))
+            else:
+                print(np.random.randint(mini, maxi+1))
 
     return
 
-def array(n: int, size: int, mini: int, maxi: int, n_flag: bool =False, size_flag: bool =False, dis: bool =False) -> None:
+def array(n: int, size: int, mini: int, maxi: int, n_flag: bool =False, size_flag: bool =False, dis: bool =False, fh, f_flag: bool =False) -> None:
     if n_flag:
-        print(n)
+        if f_flag:
+            fh.write(n)
+        else:
+            print(n)
 
     for i in range(n):
         if size_flag:
@@ -63,9 +75,12 @@ def array(n: int, size: int, mini: int, maxi: int, n_flag: bool =False, size_fla
 
     return
 
-def matrix(n: int, r: int, c: int, mini: int, maxi: int, n_flag: bool =False, nm_flag: bool =False, dis: bool =False) -> None:
+def matrix(n: int, r: int, c: int, mini: int, maxi: int, n_flag: bool =False, nm_flag: bool =False, dis: bool =False, fh, f_flag: bool =False) -> None:
     if n_flag:
-        print(n)
+        if f_flag:
+            fh.write(n)
+        else:
+            print(n)
 
     for i in range(n):
         if nm_flag:
@@ -84,9 +99,12 @@ def matrix(n: int, r: int, c: int, mini: int, maxi: int, n_flag: bool =False, nm
 
     return
 
-def string(n: int, size: int, charset: str, n_flag: bool =False, size_flag: bool =False, dis: bool =False) -> None:
+def string(n: int, size: int, charset: str, n_flag: bool =False, size_flag: bool =False, dis: bool =False, fh, f_flag: bool =False) -> None:
     if n_flag:
-        print(n)
+        if f_flag:
+            fh.write(n)
+        else:
+            print(n)
 
     for i in range(n):
         if size_flag:
@@ -107,9 +125,12 @@ def string(n: int, size: int, charset: str, n_flag: bool =False, size_flag: bool
 
     return
 
-def string_matrix(n: int, r: int, c: int, charset: str, n_flag: bool =False, nm_flag: bool =False, dis: bool =False) -> None:
+def string_matrix(n: int, r: int, c: int, charset: str, n_flag: bool =False, nm_flag: bool =False, dis: bool =False, fh, f_flag: bool =False) -> None:
     if n_flag:
-        print(n)
+        if f_flag:
+            fh.write(n)
+        else:
+            print(n)
 
     for i in range(n):
         if nm_flag:
@@ -222,15 +243,27 @@ def input_distinct() -> bool:
 
     return distinct
 
-def menu() -> int:
+def menu() -> (int, str):
     print("# Types of Test Cases #")
     print("1) Number")
     print("2) Array")
     print("3) Characters")
     print("Any-Key) Exit")
+    opt1 = int(input("Enter your option: "))
 
-    opt = int(input("Enter your option: "))
-    return opt
+    print("Should I Write it into a file?")
+    print("1) Yeah!")
+    print("2) Naah!")
+    opt2 = int(input("Should I? "))
+    if opt2 == 1:
+        fname = input("Enter your file-name: ")
+        if len(fname) == 0:
+            print("File name set to default -> test-cases.txt")
+            fname = "test-cases.txt"
+    else:
+        fname = None
+
+    return opt1, fname
 
 def input_data1() -> (int, int, int, bool, bool):
     n, n_flag = input_N()
@@ -272,11 +305,16 @@ def input_data5() -> (int, int, int, str, bool, bool, bool):
     return (n, r, c, charset, n_flag, nm_flag, dis)
 
 if __name__ == '__main__':
-    opt = menu()
+    f_flag = False
+    opt, fname = menu()
+
+    if fname != None:
+        fh = open(fname, "r")
+        f_flag = True
 
     if opt == 1:
         n, mini, maxi, n_flag, dis = input_data1()
-        numbers(n, mini, maxi, n_flag, dis)
+        numbers(n, mini, maxi, n_flag, dis, fh, f_flag)
     elif opt == 2:
         print("! Type of Array !")
         print("1) One-dimensional")
@@ -284,10 +322,10 @@ if __name__ == '__main__':
         choice = int(input("Enter your choice: "))
         if choice == 1:
             n, size, mini, maxi, n_flag, size_flag, dis = input_data2()
-            array(n, size, mini, maxi, n_flag, size_flag, dis)
+            array(n, size, mini, maxi, n_flag, size_flag, dis, fh, f_flag)
         elif choice == 2:
             n, r, c, mini, maxi, n_flag, nm_flag, dis = input_data3()
-            matrix(n, r, c, mini, maxi, n_flag, nm_flag, dis)
+            matrix(n, r, c, mini, maxi, n_flag, nm_flag, dis, fh, f_flag)
     elif opt == 3:
         print("! Type of Character Arrays !")
         print("1) Strings")
@@ -295,10 +333,12 @@ if __name__ == '__main__':
         choice = int(input("Enter your choice: "))
         if choice == 1:
             n, size, charset, n_flag, size_flag, dis = input_data4()
-            string(n, size, charset, n_flag, size_flag, dis)
+            string(n, size, charset, n_flag, size_flag, dis, fh, f_flag)
         elif choice == 2:
             n, r, c, charset, n_flag, nm_flag, dis = input_data5()
-            string_matrix(n, r, c, charset, n_flag, nm_flag, dis)
+            string_matrix(n, r, c, charset, n_flag, nm_flag, dis, fh, f_flag)
     else:
+        fh.close()
         print("Thank You, Hope to see you soon :-D")
         sys.quit()
+    fh.close()
